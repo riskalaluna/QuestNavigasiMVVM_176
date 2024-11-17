d8390ec4f95ae3dfd514223b64e4249458a24f09
riskalaluna <riskalaluna@gmail.com>
2024-11-17 10:06:25 +0700

    mengatur spasi dengan panjang 16.dp

--- app/src/main/java/com/example/praktikum6/ui/view/TampilMahasiswaView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/TampilMahasiswaView.kt
@@ -3,8 +3,10 @@ package com.example.praktikum6.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -27,6 +29,7 @@ fun TampilMahasiswaView(
         Button(onClick = {navController.popBackStack()}) {
             Text("Kembali")
         }
+        Spacer(modifier = Modifier.height(16.dp))
 
         TampilData(
             judul = "Nama",
